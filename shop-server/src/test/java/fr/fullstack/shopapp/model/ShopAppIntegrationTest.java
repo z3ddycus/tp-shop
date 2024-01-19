59c74e1c1b24c98b142c661fb0fa3733d5b316d8
yohann <yohann.henry@saagie.com>
2024-01-19 18:39:22 +0100

    feat(shop-server) Add first integration test

---
+++ shop-server/src/test/java/fr/fullstack/shopapp/model/ShopAppIntegrationTest.java
@@ -0,0 +1,19 @@
+package fr.fullstack.shopapp.model;
+
+import fr.fullstack.shopapp.ShopAppApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { ShopAppApplication.class })
+@WebAppConfiguration
+public class ShopAppIntegrationTest {
+
+    @Test
+    public void shouldStartContext() {
+
+    }
+}
